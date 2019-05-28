ac522375c622b2cfdcd064f43d737b89b29b7921
Marcel Hild <hild@b4mad.net>
2019-05-28 21:57:37 +0200

    meaningful variable names

--- model.py
+++ model.py
@@ -7,14 +7,14 @@ from sklearn.linear_model import LinearRegression
 
 def import_data_and_split():
     # Import the dataset
-    d = pandas.read_csv('data.csv')
-    x = d.iloc[:, :-1].values
-    y = d.iloc[:, 1].values
+    data_set = pandas.read_csv('data.csv')
+    x_values = data_set.iloc[:, :-1].values
+    y_values = data_set.iloc[:, 1].values
 
     # Split the dataset into the training set and test set
     # We're splitting the data in 1/3, so out of 30 rows, 20 rows will go into the training set,
     # and 10 rows will go into the testing set.
-    x_train, x_test, y_train, y_test = train_test_split(x, y, test_size =1 / 3, random_state = 0)
+    x_train, x_test, y_train, y_test = train_test_split(x_values, y_values, test_size =1 / 3, random_state = 0)
 
     return (x_train, x_test, y_train, y_test)
 
